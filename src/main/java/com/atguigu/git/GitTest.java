6e91c96753db9eb3fdd289fbdad564c64608635e
Hyp_0226 <1548387739@qq.com>
2022-12-09 21:17:49 +0800

    first IDEA commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author:Hyp_0226
+ * @data 2022/12/9 0009 - 21:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
